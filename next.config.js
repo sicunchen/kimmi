2bbd1ece535c5a5d7037537e53a6e5e0c699a1af
lia chen <liachen@may-liachen-MacBook-Air.local>
2023-10-20 15:32:05 -0400

    revert output

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const path = require('path');
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
   swcMinify: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
